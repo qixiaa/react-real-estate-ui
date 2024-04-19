c907b0bba4cd664613c02d67b4dbbf3bad983159
qixiaa <aqixiaa@gmail.com>
2024-04-19 17:09:20 +0800

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>LamaEstate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign in</a>
+        <a href="/" className="register">
+          Sign up
+        </a>
+
+        <div className="menuIcon">
+          <img src="/menu.png" alt="" />
+        </div>
+        <div className="menu">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
