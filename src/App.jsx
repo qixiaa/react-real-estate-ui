714eadf6b1f9d5896338023286b638d2f605f472
qixiaa <aqixiaa@gmail.com>
2024-04-22 14:39:49 +0800

    completed real-estate ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import HomePage from "./pages/homePage/homePage";
+import ListPage from "./pages/listPage/listPage";
+import Layout from "./pages/layout/layout";
+import SinglePage from "./pages/singlePage/singlePage";
+import ProfilePage from "./pages/profilePage/profilePage";
 
 function App() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+function Navbar() {
   const [open, setOpen] = useState(false);
+
+  const user = true;
   return (
     <nav>
       <div className="left">
@@ -16,11 +19,26 @@ const Navbar = () => {
         <a href="/">Agents</a>
       </div>
       <div className="right">
-        <a href="/">Sign in</a>
-        <a href="/" className="register">
-          Sign up
-        </a>
-
+        {user ? (
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            <span>John Doe</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <a href="/">Sign in</a>
+            <a href="/" className="register">
+              Sign up
+            </a>
+          </>
+        )}
         <div className="menuIcon">
           <img
             src="/menu.png"
@@ -39,6 +57,6 @@ const Navbar = () => {
       </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
